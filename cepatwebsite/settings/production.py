9eaa5c04613d233b695f2dcf658dd94778aba507
unknown <eremugomark@gmail.com>
2019-01-08 01:58:18 +0300

    all initial commit

--- cepatwebsite/settings/production.py
+++ cepatwebsite/settings/production.py
@@ -133,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "/www.cepatwebsite.herokuapp.com/static/"
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'cepat/media')
 MEDIA_URL = '/media/'
